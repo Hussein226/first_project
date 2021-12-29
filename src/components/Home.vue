aec4f99f0579714a8a3a8c2ff1582d9a169fd608
unknown <houranihussein@gmail.com>
2021-12-29 15:57:33 -0500

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Hello {{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            message: 'Hello Word'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
