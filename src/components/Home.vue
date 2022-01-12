43e033e6290d2f41cabef806f7757ef1c3a89639
huss <houranihussein@gmail.com>
2022-01-12 17:10:55 -0500

    Added axios post to localhost spring backend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <v-form
+      @submit="submitForm"
+    >
+      <v-text-field
+        v-model='firstName'
+        label='First Name'
+      >
+      </v-text-field>
+      <v-text-field
+        v-model='lastName'  
+        label='Last Name'
+      >
+      </v-text-field>
+      <v-text-field
+        v-model='age'
+        label='Age'  
+      >
+      </v-text-field>
+      <v-text-field
+        v-model='gender'
+        label='Gender'  
+      >
+      </v-text-field>
+      <v-btn
+        type="submit"
+      >
+        Submit Form
+      </v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { setUserData } from '../services/BackEndService'
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      age: '',
+      gender: ''
+    }
+  },
+  methods: {
+    async submitForm() {
+     let requestBody = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        age: this.age,
+        gender: this.gender
+      }
+      console.log(requestBody)
+
+      try{
+        let response = await setUserData(requestBody)
+
+        return response
+      }catch(error){
+        console.log(error)
+      }
+      
+    }
+  }
 }
 </script>
